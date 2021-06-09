fae241aa9d28c655767cd57acd8e0d077b4ee798
iganeshbalu <iganeshbalu@DESKTOP-JSPSNEM>
2021-06-09 15:03:25 +0300

    Add project files.

---
+++ SystemAudioDemo/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace SystemAudioDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RecordButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RecordButton
+            // 
+            this.RecordButton.Location = new System.Drawing.Point(241, 172);
+            this.RecordButton.Name = "RecordButton";
+            this.RecordButton.Size = new System.Drawing.Size(125, 58);
+            this.RecordButton.TabIndex = 0;
+            this.RecordButton.Text = "Record";
+            this.RecordButton.UseVisualStyleBackColor = true;
+            this.RecordButton.Click += new System.EventHandler(this.RecordButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(394, 172);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(125, 58);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.RecordButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RecordButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ SystemAudioDemo/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.IO;
+using System.Diagnostics;
+
+namespace SystemAudioDemo
+{
+    public partial class Form1 : Form
+    {
+        private string outputFileName;
+        private WasapiLoopbackCapture capture;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RecordButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "Wave files | *.wav";
+
+            if (dialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            outputFileName = dialog.FileName;
+
+            RecordButton.Enabled = false;
+            StopButton.Enabled = true;
+
+            capture = new WasapiLoopbackCapture();
+            var writer = new WaveFileWriter(outputFileName, capture.WaveFormat);
+
+            capture.DataAvailable += async (s, e) =>
+            {
+                if (writer != null)
+                {
+                    await writer.WriteAsync(e.Buffer, 0, e.BytesRecorded);
+                    await writer.FlushAsync();
+                }
+            };
+
+            capture.RecordingStopped += (s, e) =>
+            {
+                if (writer != null)
+                {
+                    writer.Dispose();
+                    writer = null;
+                }
+
+                RecordButton.Enabled = true;
+                capture.Dispose();
+            };
+
+            capture.StartRecording();
+
+
+
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            StopButton.Enabled = false;
+            capture.StopRecording();
+
+            if (outputFileName == null)
+                return;
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = Path.GetDirectoryName(outputFileName),
+                UseShellExecute = true
+            };
+
+            Process.Start(processStartInfo);
+        }
+    }
+}
